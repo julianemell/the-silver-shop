ab54d173c2f429d375f805bba04ce3672a7a983c
Julia Nemell <jnemell@gmail.com>
2022-12-27 12:20:25 +0100

    Adds the logo and favicon to the project

--- web/components/Layout.js
+++ web/components/Layout.js
@@ -1,13 +1,20 @@
-import Footer from "./Footer"
+import Head from 'next/head'
 import Nav from "./Nav"
+import Footer from "./Footer"
 
 const Layout = ({ children }) => {
 	return (
-		<div className='content'>
-			<Nav />
-			{ children }
-			<Footer />
-		</div>
+		<>
+			<Head>
+				<title>Julias Shop of Silver</title>
+			</Head>
+
+			<div className='content'>
+				<Nav />
+				{ children }
+				<Footer />
+			</div>
+		</>
 	)
 }
 
