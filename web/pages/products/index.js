b14fd5849513196986d393c95ed89e60c1d19fd1
Julia Nemell <jnemell@gmail.com>
2022-12-27 14:22:56 +0100

    moves the fetching of the products to the productsPage and fetches the products in the getStaticProps and renders them in a list

--- web/pages/products/index.js
+++ web/pages/products/index.js
@@ -1,8 +1,32 @@
+import { createClient } from 'next-sanity'
 
+const client = createClient({
+	projectId: 'fmf3b9s8',
+	dataset: 'production'
+})
 
-const Products = () => {
+export async function getStaticProps() {
+	const products = await client.fetch(`*[_type == "product"]`);
+
+	return {
+		props: {
+			products
+		}
+	};
+}
+
+const Products = ({ products }) => {
 	return (
-		<div>Products</div>
+		<div>
+			{products.length > 0 && (
+				<ul>
+					{products.map((product) => (
+					<li key={product._id}>{product?.name}</li>
+					))}
+				</ul>
+			)}
+			{!products.length > 0 && <p>No products to show</p>}
+		</div>
 	)
 }
 
