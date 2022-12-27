4df6b32f739fbe00ce0d14f83cac47401d6a4642
Julia Nemell <jnemell@gmail.com>
2022-12-27 15:56:04 +0100

    removes the id from list

--- web/components/ProductCard.js
+++ web/components/ProductCard.js
@@ -4,7 +4,6 @@ const ProductCard = ({ product }) => {
 	return (
 		<li key={product._id}>
 			<Link href={`/products/${product._id}`}>{product?.name}</Link>
-			<p>{product._id}</p>
 		</li>
 	)
 }
