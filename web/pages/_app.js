e2b38f4997ac70c751c40573a5bdffe8a5570aa4
Julia Nemell <jnemell@gmail.com>
2022-12-27 11:27:31 +0100

    adds the layout component to the app.js

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,5 +1,12 @@
+import Layout from '../components/Layout'
 import '../styles/main.scss'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	)
 }
+
+export default App
