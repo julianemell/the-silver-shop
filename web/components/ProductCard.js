81417ef5f048616d2c4cd0cc3615ec97a6baf55d
Julia Nemell <jnemell@gmail.com>
2022-12-28 13:50:00 +0100

    adds image, cost and rearranges the card and adds classes for styling

--- web/components/ProductCard.js
+++ web/components/ProductCard.js
@@ -1,10 +1,18 @@
+import { urlFor } from '../library/client'
 import Link from 'next/link'
 
 const ProductCard = ({ product }) => {
+	console.log('product', product)
 	return (
-		<li key={product._id}>
-			<Link href={`/products/${product._id}`}>{product?.name}</Link>
-		</li>
+		<Link className='product__card' href={`/products/${product._id}`} key={product._id}>
+			
+				{product.images?.map(image => (
+					<img key={image?.asset._ref} src={urlFor(image).url()} alt='' width='200' className='product__image' />
+				))}
+				<h2>{product?.name}</h2>
+				<p>{product.productCost} kr</p>
+			
+		</Link>
 	)
 }
 
