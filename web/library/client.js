ccc7f4f0a92f438a9cd18a04756ee7eee9b31e17
Julia Nemell <jnemell@gmail.com>
2022-12-28 12:28:23 +0100

    apparently the keys have to be named NEXT_PUBLIC

--- web/library/client.js
+++ web/library/client.js
@@ -3,11 +3,11 @@ import imageUrlBuilder from '@sanity/image-url'
 
 // connection to sanity
 export const client = createClient({
-	projectId: process.env.SANITY_PROJECT_ID,
-	dataset: process.env.SANITY_DATASET,
+	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
 	apiVersion: '2022-12-20',
 	useCdn: true,
-	token: process.env.SANITY_TOKEN,
+	token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 })
 
 // so that we can use sanity images
