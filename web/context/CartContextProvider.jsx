84134b1c3f709b201fc8cc8d6cc53446575f6577
Julia Nemell <jnemell@gmail.com>
2022-12-30 11:39:15 +0100

    adds context for the cart so that we can access quantity and products in cart around the app

---
+++ web/context/CartContextProvider.jsx
@@ -0,0 +1,76 @@
+import { useContext, createContext, useState } from 'react'
+
+export const CartContext = createContext({})
+
+export const useShoppingCart = () => {
+	return useContext(CartContext)
+}
+
+const CartContextProvider = ({ children }) => {
+	const [cartItems, setCartItems] = useState([])
+	console.log('cartItems', cartItems)
+	
+	const getItemQuantity = (id) => {
+		console.log('cartItems get item quantity', cartItems)
+		if(cartItems.find(item => item.id === id)){
+			return + 1
+		} else {
+			return 0
+		}
+	}
+
+	const increaseCartQuantity = (id) => {
+		setCartItems(currentItems => {
+			if(currentItems.find(item => item.id === id) == null) {
+				return [...currentItems, { id, quantity: 1 }]
+			} else {
+				return currentItems.map(item => {
+					if (item.id === id) {
+						return { ...item, quantity: item.quantity + 1}
+					} else {
+						return item
+					}
+				})
+			}
+		})
+	}
+
+	const decreaseCartQuantity = (id) => {
+		console.log('id', id)
+		setCartItems(currentItems => {
+			console.log('cartitems', currentItems)
+			if(currentItems.find(item => item.id === id)) {
+				return currentItems.pop(item => item.id !== id)
+			} else {
+				return currentItems.map(item => {
+					if (item.id === id) {
+						return { ...item, quantity: item.quantity - 1}
+					} else {
+						return item
+					}
+				})
+			}
+		})
+	}
+
+	const removeFromCart = (id) => {
+		setCartItems(currentItems => {
+			return currentItems.filter(item => item.id !== id)
+		})
+	}
+
+	const values = {
+		getItemQuantity, 
+		increaseCartQuantity, 
+		decreaseCartQuantity, 
+		removeFromCart,
+	}
+
+	return (
+		<CartContext.Provider value={values}>
+			{children}
+		</CartContext.Provider>
+	)
+}
+
+export default CartContextProvider
