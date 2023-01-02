3d3bb15ba6aa329f97450d169d6f186de1c90bb7
Julia Nemell <jnemell@gmail.com>
2023-01-02 10:42:49 +0100

    adds classes to the shoppingcart items etc and adds button

--- web/components/ShoppingCart.js
+++ web/components/ShoppingCart.js
@@ -1,11 +1,27 @@
 import { useShoppingCart } from '../context/CartContextProvider'
 
 const ShoppingCart = () => {
-	const { closeCart } = useShoppingCart()
+	const { closeCart, cartItems } = useShoppingCart()
+	console.log('cartItems i shoppingcart', cartItems)
 
 	return (
-		<div>ShoppingCart</div>
+		<div className='shoppingcart'>
+			<div className='shoppingcart-items'>
+				<div>
+					{cartItems && cartItems.map(item => (
+						<div className='shoppingcart-item'>
+							<p>{item.id} / {item.quantity}</p>
+						</div>
+					))}
+					<div>
+						<button className='button button--secondary'>Go to checkout</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	)
 }
+
+
 
 export default ShoppingCart
