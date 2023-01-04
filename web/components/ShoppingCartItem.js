cd8a9c0d7ab6c643db42b24fa710862d5df513ff
Julia Nemell <jnemell@gmail.com>
2023-01-04 17:28:52 +0100

    fetches all the products in useEffect so that we can compare the id and check which products that are in the basket

--- web/components/ShoppingCartItem.js
+++ web/components/ShoppingCartItem.js
@@ -1,19 +1,43 @@
 import { useShoppingCart } from '../context/CartContextProvider'
+import { client, urlFor } from '../library/client'
+import { useEffect, useState } from 'react'
 
 const ShoppingCartItem = ({ product }) => {
+	const [allProducts, setAllProducts] = useState([])
+	
+	const productInCart = allProducts.find(i => i._id === product.id)
+	
 	const {
+		getItemQuantity,
 		increaseCartQuantity,
 		decreaseCartQuantity,
 		removeFromCart,
 	} = useShoppingCart()
 
+	const quantity = getItemQuantity(product.id)
+
+	useEffect(() => {
+		const getProducts = async () => {
+			await client
+				.fetch(`*[_type == "product"]`)
+				.then(product => setAllProducts(product))
+		}
+		getProducts()
+	}, [])
+
 	return (
 		<div className='shoppingcart-item'>
-			<p>{product.id}</p>
+			{productInCart && 
+				<p>{productInCart.name}</p>
+				
+			}
+			{productInCart && productInCart.images.map(image => (
+				<img key={image?.asset._ref} src={urlFor(image).url()} alt='' width='100' />
+			))}
 			<span className='shoppingcart--add-remove'>
 				<div className='product--amount'>
 					<button className='product--amount-change' onClick={() => decreaseCartQuantity(product.id)}>-</button>
-					<span>{product.quantity}</span>
+					<span>{quantity}</span>
 					<button className='product--amount-change' onClick={() => increaseCartQuantity(product.id)}>+</button>
 				</div>
 				<button className='shoppingcart--remove' onClick={() => removeFromCart(product.id)}></button>
