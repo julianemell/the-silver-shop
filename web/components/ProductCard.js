7e5900dcf1c285064d0b1fcdb56ac196972e3ca8
Julia Nemell <jnemell@gmail.com>
2023-01-09 13:33:02 +0100

    product card shows only first uploaded image if there is many

--- web/components/ProductCard.js
+++ web/components/ProductCard.js
@@ -2,13 +2,13 @@ import { urlFor } from '../library/client'
 import Link from 'next/link'
 
 const ProductCard = ({ product }) => {
-	console.log('product', product)
+	
 	return (
 		<Link className='product__card' href={`/products/${product.productSlug.current}`} key={product._id}>
 			
-				{product.images?.map(image => (
-					<img key={image?.asset._ref} src={urlFor(image).url()} alt='' width='200' className='product__image' />
-				))}
+				{product.images && 
+					<img key={product.images[0].asset._ref} src={urlFor(product.images[0]).url()} alt='' width='200' className='product__image' />
+				}
 				<h2>{product?.name}</h2>
 				<p>{product.productCost} kr</p>
 			
