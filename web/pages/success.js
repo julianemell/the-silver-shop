b6e457e6ea29cb7ed376e5bb469c09569ffa8757
Julia Nemell <jnemell@gmail.com>
2023-01-10 11:01:29 +0100

    adds success page with a thank you and more and clears the cartitems and localstorage

---
+++ web/pages/success.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { useShoppingCart } from '../context/CartContextProvider'
+
+const success = () => {
+	const [order, setOrder] = useState(null)
+	const { setCartItems } = useShoppingCart()
+
+	useEffect(() => {
+		localStorage.clear()
+		setCartItems([])
+	}, [])
+
+	return (
+		<div className=''>
+			<h2>Thank you for your order!</h2>
+			<p>Check your email for the order confirmation</p>
+			<p>If you have any questions please email: 
+				<a href='mailto:order@juliasshopofsilver.com'> order@juliasshopofsilver.com</a>
+			</p>
+			<Link href='/'>
+				<button className='button button--primary'>
+					Continue shopping
+				</button>
+			</Link>
+		</div>
+	)
+}
+
+export default success
