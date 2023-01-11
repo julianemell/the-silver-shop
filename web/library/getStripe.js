3019ca94ee0307ce65d856c5dd6cf994371d1673
Julia Nemell <jnemell@gmail.com>
2023-01-11 12:22:51 +0100

    changes to the publishable key again

--- web/library/getStripe.js
+++ web/library/getStripe.js
@@ -1,10 +1,11 @@
 import { loadStripe } from '@stripe/stripe-js'
 
 let stripePromise
+const apiKey = `${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`
 
 const getStripe = () => {
 	if(!stripePromise) {
-		stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`)
+		stripePromise = loadStripe(apiKey)
 	}
 
 	return stripePromise
