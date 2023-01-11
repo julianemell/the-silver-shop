0db866104b44451f4284b2796608c1fb1565d840
Julia Nemell <jnemell@gmail.com>
2023-01-11 13:19:22 +0100

    changes the footer to more of a navbar

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -1,6 +1,30 @@
+import Link from "next/link"
+
 const Footer = () => {
 	return (
-		<footer className='footer'>Copyright ® Julias Shop of Silver 2023</footer>
+		//<footer className='footer'>Copyright ® Julias Shop of Silver 2023</footer>
+		<nav className='nav-footer'>
+			<Link href='/products' className='nav-footer__item nav-footer__item-shop'>
+				<div>
+					Shop
+				</div>
+			</Link>
+			<Link href='/about' className='nav-footer__item nav-footer__item-about'>
+				<div>
+					About
+				</div>
+			</Link>
+			<Link href='#' className='nav-footer__item nav-footer__item-social'>
+				<div>
+					instagram
+				</div>
+			</Link>
+			<Link href='#' className='nav-footer__item nav-footer__item-mail'>
+				<div>
+					mailing list
+				</div>
+			</Link>
+		</nav>
 	)
 }
 
