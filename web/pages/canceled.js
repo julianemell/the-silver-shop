c009a288b51d11e590d7131b9eb44898dd49acea
Julia Nemell <jnemell@gmail.com>
2023-01-11 10:59:40 +0100

    adds cancelled page, if you dont fulfill your payment

---
+++ web/pages/canceled.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const canceled = () => {
+	const router = useRouter()
+
+	//redirect to the homepage in 3 seconds
+	useEffect(() => {
+		setTimeout(() => {
+			router.push('/products') 
+		}, 3000)
+	}, [])
+
+	return (
+		<div>
+			<p>No order has gone through</p>
+			<p>You are being redirected to the products page</p>
+		</div>
+	)
+}
+
+export default canceled
