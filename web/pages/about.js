e16558e8ac69bb274847a1a238145358b8ee25aa
Julia Nemell <jnemell@gmail.com>
2023-01-12 14:45:07 +0100

    uses the PortableText component so that we can render the text from aboutText

--- web/pages/about.js
+++ web/pages/about.js
@@ -1,14 +1,31 @@
 import { client, urlFor } from '../library/client'
+import { PortableText } from '@portabletext/react'
 
 const about = ({ about }) => {
 	const { aboutHeading, aboutText, image } = about
-	console.log('image', image)
+
+	const portableTextComponents = {
+		list: {
+			bullet: ({ children }) => {return (<ul>{ children }</ul>)},
+			number: ({ children }) => {return (<ol>{ children }</ol>)}
+		},
+		block: {
+			normal: ({ children }) => {return (<p>{ children }</p>)},
+			h2: ({ children }) => {return (<h2>{ children }</h2>)},
+			h3: ({ children }) => {return (<h3>{ children }</h3>)},
+			blockquote: ({ children }) => {return (<blockquote className='quote'>{ children }</blockquote>)},
+		},
+		marks: {
+			link: ({ value, children }) => {return (<a href={value?.href} target='_blank'>{ children }</a>)}
+		}
+	}
+
 	return (
 		<div className='about'>
+
 			<h1>{aboutHeading}</h1>
-			{aboutText && aboutText.map((content, i) => (
-				<p key={i}>{content.children[0].text}</p>
-			))}
+			<PortableText value={aboutText} components={portableTextComponents} />
+
 			{image && (
 				<img key={image.asset._ref} src={urlFor(image).url()} alt={image.attribution} className='about__image' />
 			)}
