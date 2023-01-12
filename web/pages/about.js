52646c77de29dd81b7a8c45ee63159b6a6c74044
Julia Nemell <jnemell@gmail.com>
2023-01-12 11:00:38 +0100

    renders content from about in sanity

--- web/pages/about.js
+++ web/pages/about.js
@@ -1,15 +1,20 @@
-import { client } from '../library/client'
+import { client, urlFor } from '../library/client'
 
 const about = ({ about }) => {
-	const { aboutHeading, aboutText } = about
+	const { aboutHeading, aboutText, image } = about
+	console.log('image', image)
 	return (
-		<div>
+		<div className='about'>
 			<h1>{aboutHeading}</h1>
-			<p>{aboutText}</p>
+			{aboutText && aboutText.map((content, i) => (
+				<p key={i}>{content.children[0].text}</p>
+			))}
+			{image && (
+				<img key={image.asset._ref} src={urlFor(image).url()} alt={image.attribution} className='about__image' />
+			)}
 		</div>
 	)
 }
-
 
 export const getServerSideProps = async () => {
 	const about = await client.fetch(`*[_type == "about"][0]`)
