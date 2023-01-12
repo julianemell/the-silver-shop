7c6c530a839deb7b07697b099c982d58a9bc1861
Julia Nemell <jnemell@gmail.com>
2023-01-12 14:44:17 +0100

    adds to the about schema with a custom wysiwyg

--- studio/schemas/about.js
+++ studio/schemas/about.js
@@ -16,7 +16,42 @@ export default {
 			title: 'About Text',
 			name: 'aboutText',
 			type: 'array',
-			of: [{ type: 'block' }],
+			of: [
+				{
+					type: 'block', 
+					styles: [
+						{title: 'Normal', value: 'normal'},
+						{title: 'H2', value: 'h2'},
+						{title: 'H3', value: 'h3'},
+						{title: 'Quote', value: 'blockquote'},
+					], 
+					marks: { 
+						decorators: [
+							{title: 'Strong', value: 'strong'},
+							{title: 'Emphasis', value: 'em'},
+							{title: 'Underline', value: 'underline'},
+						],
+						annotations: [
+							{
+								title: 'External link',
+								name: 'link',
+								type: 'object',
+								fields: [
+									{
+										title: 'URL',
+										name: 'href',
+										type: 'url',
+									}
+								]
+							}
+						],
+					}, 
+					lists: [
+						{title: 'Bullet', value: 'bullet'},
+						{title: 'Numbered', value: 'number'},
+					] 
+				}
+			],
 			validation: Rule => Rule.required(),
 		},
 		{
