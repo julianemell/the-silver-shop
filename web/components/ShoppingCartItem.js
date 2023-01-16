595273a7b25163ad068cdbe54f718c81c885227a
Julia Nemell <jnemell@gmail.com>
2023-01-16 18:48:06 +0100

    adds quantity to increaseCartQuantity. shows message when you cant add more items (because stock is low)

--- web/components/ShoppingCartItem.js
+++ web/components/ShoppingCartItem.js
@@ -2,7 +2,6 @@ import { useShoppingCart } from '../context/CartContextProvider'
 import { urlFor } from '../library/client'
 
 const ShoppingCartItem = ({ product, allProducts }) => {
-	
 	const productInCart = allProducts.find(i => i._id === product.id)
 	
 	const {
@@ -10,6 +9,7 @@ const ShoppingCartItem = ({ product, allProducts }) => {
 		increaseCartQuantity,
 		decreaseCartQuantity,
 		removeFromCart,
+		stockMessage,
 	} = useShoppingCart()
 
 	const quantity = getItemQuantity(product.id)
@@ -18,37 +18,37 @@ const ShoppingCartItem = ({ product, allProducts }) => {
 		return cost * amount
 	}
 
-
 	return (
 		<div className='shoppingcart-item'>
-			{productInCart && 
-				<p>{productInCart.name}</p>
-				
-			}
+			{productInCart && (
+				<>
+					<div className='shoppingcart-item__img-name-price'>
+						<span className='shoppingcart-item__img'>
+							{productInCart.images && 
+								<img 
+									key={productInCart.images[0].asset._ref}
+									src={urlFor(productInCart.images[0]).url()}
+									alt=''
+									width='50'
+								/>
+							}
+						</span>
+						<p>{productInCart.name}</p>
+						<p className='shoppingcart-item__price'>{totCost(productInCart.productCost, quantity)} kr</p>
+					</div>
+					
+					<span className='shoppingcart--add-remove'>
+						<div className='product--amount'>
+							<button className='product--amount-change' onClick={() => decreaseCartQuantity(product.id)}>-</button>
+							<span>{quantity}</span>
+							<button className='product--amount-change' onClick={() => increaseCartQuantity(product.id, product, (quantity + 1))}>+</button>
+						</div>
+						<button className='shoppingcart--remove' onClick={() => removeFromCart(product.id)}></button>
+					</span>
 
-			{productInCart.images && 
-				<img 
-					key={productInCart.images[0].asset._ref} 
-					src={urlFor(productInCart.images[0]).url()} 
-					alt='' 
-					width='50' 
-					className='' 
-				/>
-			}
-
-			<span className='shoppingcart--add-remove'>
-				<div className='product--amount'>
-					<button className='product--amount-change' onClick={() => decreaseCartQuantity(product.id)}>-</button>
-					<span>{quantity}</span>
-					<button className='product--amount-change' onClick={() => increaseCartQuantity(product.id, product)}>+</button>
-				</div>
-
-				{productInCart && (
-					<p>{totCost(productInCart.productCost, quantity)} kr</p>
-				)}
-
-				<button className='shoppingcart--remove' onClick={() => removeFromCart(product.id)}></button>
-			</span>
+					{product.message !== '' && <p className='stocklevel-message'>{product.message}</p>}
+				</>
+			)}
 		</div>
 	)
 }
