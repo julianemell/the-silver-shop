18f9c8c89351639a3156ee51a6222d281a12d4c3
Julia Nemell <jnemell@gmail.com>
2023-01-20 10:54:50 +0100

    fixes the logo on the home page

--- web/pages/index.js
+++ web/pages/index.js
@@ -4,8 +4,10 @@ export default function Home() {
 	return (
 		<>
 			<main className='home'>
-				<div className='home__logo animate__animated animate__bounceInDown'>
-					<img src='/shop-of-silver.svg' />
+				<div className='home__logo'>
+					<div>
+						<img className='animate__animated animate__bounceInDown' src='/shop-of-silver.svg' />
+					</div>
 				</div>
 			</main>
 		</>
